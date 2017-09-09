2911a229e63afab7a2c1960b30adc07470b5358c
mpdeep <31785460+mpdeep@users.noreply.github.com>
2017-09-09 11:24:05 +0530

    Update WordCountDriver.java

--- WordCountDriver.java
+++ WordCountDriver.java
@@ -18,7 +18,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.bigtable.sample;
+package com.google.cloud.hadoop.io.bigquery.samples;
 
 import org.apache.hadoop.hbase.mapreduce.Export;
 import org.apache.hadoop.util.ProgramDriver;
